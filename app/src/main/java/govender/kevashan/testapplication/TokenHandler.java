aac95a891946f510fdd6bedf484044422585a8eb
Kevashan Govender <kevashan.govender@standardbank.co.za>
2017-02-14 09:43:35 +0200

    First Commit

---
+++ app/src/main/java/govender/kevashan/testapplication/TokenHandler.java
@@ -0,0 +1,94 @@
+package govender.kevashan.testapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.http.SslError;
+import android.os.AsyncTask;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by kevashan on 2017/02/13.
+ */
+
+public class TokenHandler {
+
+    private WebView webView;
+
+    private boolean caughtToken = false;
+
+
+    public String getToken() {
+
+        return token;
+    }
+
+    public boolean isCaughtToken() {
+        return caughtToken;
+    }
+
+    private String token;
+
+    public TokenHandler(WebView webView) {
+
+        this.webView = webView;
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+    }
+
+    public void setWebViewClient() {
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                if (webView.getUrl().contains("#access_token")) {
+
+                    updateToken();
+
+                    System.out.println("Kevashan " + token);
+
+
+                    //RESTLayer restLayer = new RESTLayer(webView);
+                    //restLayer.POSTAuthorize();
+
+                    //System.out.println("POSTAuthorize ran");
+                    //System.out.println("POSTAuthorize url: " + webView.getUrl());
+
+                    //updateToken();
+
+                    //restLayer.POSTExecute();
+                    //System.out.println("POSTExecute ran");
+
+                   // updateToken();
+
+                    //restLayer.POSTRegister();
+                    //System.out.println("POSTRegister ran");
+
+                }
+            }
+        });
+    }
+
+    public void loadURL() {
+
+        webView.loadUrl("https://testappmanager.standardbank.co.za/app.html");
+    }
+
+    public void updateToken(){
+
+        String URL = webView.getUrl();
+        token = URL.substring(URL.indexOf("access_token=") + 13, URL.indexOf("&expires_in="));
+
+    }
+
+}
